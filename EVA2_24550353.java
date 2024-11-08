e83004de9855789b009cba8a1e50dca2012fc4cb
SAMANTA-ABIGAIL-SANCHEZ-JUAREZ <L24550353@chihuahua2.tecnm.mx>
2024-11-08 17:04:26 -0600

    Add files via upload

---
+++ EVA2_24550353.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_24550353;
+
+import com.sun.source.tree.BreakTree;
+
+/**
+ *
+ * @author sanch
+ */
+public class EVA2_24550353 {
+
+    public static void main(String[] args) {
+         
+       int piedra = 0;
+       int papel = 0;
+       int tijera = 0;
+    int i;
+        if(piedra==1)
+        System.out.println("Piedra");
+        if(papel==2)
+        System.out.println("Papel");
+         if(tijera==3)
+        System.out.println("Tijera");
+    }
+    
+}
+
